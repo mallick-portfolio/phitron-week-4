e807104c39948d44dd559118ad6e192a9ecb19bc
mallick-portfolio <tamal1714019@gmail.com>
2023-12-07 21:10:56 +0600

    added related name for category model

--- assignment/category/models.py
+++ assignment/category/models.py
@@ -3,7 +3,7 @@ from task.models import Task
 # Create your models here.
 class Category(models.Model):
   name = models.CharField(max_length=50)
-  task = models.ManyToManyField(Task)
+  task = models.ManyToManyField(Task, related_name='categories')
 
   def __str__(self):
       return self.name
